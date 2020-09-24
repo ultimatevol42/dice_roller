87a9441daa6c4f9dfe9738f141b8a8b08195d5f5
Christopher Haley <ultimatevol42@gmail.com>
2020-09-24 15:24:53 -0500

    update

--- assets/Javascript/main.js
+++ assets/Javascript/main.js
@@ -1,32 +1,36 @@
-const rollButton = document.querySelector('#roll-button')
-let dieRoll = []
-const numberofDice = document.querySelector('#number-of-dice')
-const total = document.querySelector('#TotalRolls')
-rollButton.addEventListener("click" , function (){
-    let numberofRolledDice = numberofDice.value;
-    console.log(numberofRolledDice)
-    let dice = 0 
-    while (dice < numberofRolledDice) {
-        dieRoll.push(Math.floor(Math.random() * 6 +1))
-        dice += 1
-    }
-    console.log(dieRoll)
-    let sum = dieRoll.reduce(function (a,b){
-        return a + b;
-    }, 0);
-   console.log(sum)
-   total.innerHTML = "Total Roll "  +sum
-})
-const showAllRolls = document.querySelector('#Show-all-Rolls')
-const allRollsList = document.querySelector('#All-Rolls')
-showAllRolls.addEventListener("click", function () {
-    for (let i=0; i < dieRoll.length; i++){
-        allRollsList.innerHTML += "<li class='dice'>" + dieRoll[i] +"</li>"
-    }
-})
-const resetButton = document.querySelector("Reset-button")
-resetButton.addEventListener("click", function() {
-    numberofDice.value = ""
-    allRollsList.innerHTML = ""
-    dieRoll =[]
-})
+const rollButton = document.querySelector("#roll-button");
+let dieRoll = [];
+const total = document.querySelector("#Total-Number-Rolls");
+rollButton.addEventListener("click", function () {
+  let numberofDice = document.getElementById("number-of-dice").value;
+  let numberofRolledDice = numberofDice;
+  console.log(numberofRolledDice);
+
+  let dice = 0;
+  while (dice < numberofRolledDice) {
+    dieRoll.push(Math.floor(Math.random() * 6 + 1));
+    dice += 1;
+  }
+  console.log(dieRoll);
+
+  let sum = dieRoll.reduce(function (a, b) {
+    return a + b;
+  }, 0);
+  console.log(sum);
+  total.innerHTML = "Total Roll " + sum;
+});
+
+let ShowAllRolls = document.getElementById("Show-All-Rolls");
+const allRollsList = document.querySelector("#All-Rolls");
+ShowAllRolls.addEventListener("click", function () {
+  for (let i = 0; i < dieRoll.length; i++) {
+    allRollsList.innerHTML += "<li class='dice'>" + dieRoll[i] + "</li>";
+  }
+});
+
+const resetButton = document.getElementById("Reset-button");
+resetButton.addEventListener("click", function () {
+  total.innerHTML = "";
+  allRollsList.innerHTML = "";
+  dieRoll = [];
+});
